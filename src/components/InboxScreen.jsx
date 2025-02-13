1e4be23e9b412436a6a5d6740418536eda326155
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2025-02-13 23:38:44 +0200

    change back to origin, cicd work

--- src/components/InboxScreen.jsx
+++ src/components/InboxScreen.jsx
@@ -20,8 +20,6 @@ export default function InboxScreen() {
       <div className="page lists-show">
         <div className="wrapper-message">
           <span className="icon-face-sad" />
-          <span className="icon-face-sad" />
-          <span className="icon-face-sad" />
           <p className="title-message">Oh no!</p>
           <p className="subtitle-message">Something went wrong</p>
         </div>
