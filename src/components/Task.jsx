be9d2a229a5ea907f92cf1014734c9177f724ef4
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2025-02-13 23:43:03 +0200

    change task backgroudn to red

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -30,6 +30,7 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
+          style={{ backgroundColor: "red" }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
