af1f661764a23985710204bd424243886a73f235
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2025-02-13 23:25:52 +0200

    add icon-face-sad-tear when error

--- src/components/InboxScreen.jsx
+++ src/components/InboxScreen.jsx
@@ -20,6 +20,7 @@ export default function InboxScreen() {
       <div className="page lists-show">
         <div className="wrapper-message">
           <span className="icon-face-sad" />
+          <span className="icon-face-sad-tear" />
           <p className="title-message">Oh no!</p>
           <p className="subtitle-message">Something went wrong</p>
         </div>
